dc8f56cb692ac5dbe5a2e85a1d4d7525ac882045
Nathan Zadoks <nathan@nathan7.eu>
2013-03-02 14:43:30 +0100

    don't abuse exposed private vars from EventEmitter

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function listenUrl() {
     , host = (addr.family == 'IPv6') ? ('[' + addr.address + ']') : addr.address
     , port = addr.port
     , url = 'http://' + host + (port == 80 ? '' : (':' + port))
-  if (this._events && this._events.url && typeof this.emit == 'function')
+  if (typeof this.listeners === 'function' && (this.listeners('url') || []).length)
     return this.emit('url', url, { host: host, port: port }, addr)
   console.log('listening on ' + url)
 }
