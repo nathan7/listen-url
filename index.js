6d9a48d317378e0f0b1d48f3e360e9442586dabb
Nathan Zadoks <nathan@nathan7.eu>
2013-03-10 15:14:59 +0100

    handle event emission even more gracefully

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function listenUrl() {
     , host = (addr.family == 'IPv6') ? ('[' + addr.address + ']') : addr.address
     , port = addr.port
     , url = 'http://' + host + (port == 80 ? '' : (':' + port))
-  if (typeof this.listeners === 'function' && (this.listeners('url') || []).length)
-    return this.emit('url', url, { host: host, port: port }, addr)
+  if (this.emit)
+    if (this.emit('url', url, { host: host, port: port }, addr)) return
   console.log('listening on ' + url)
 }
